2fdacac88e8d70f87999b7b8d874474228e0d11a
Mriganka Dhar <mrigankodhar0001@gmail.com>
2025-02-10 10:15:32 +0530

    Add gRPC gateway and service implementation.

--- gateway/http_handler.go
+++ gateway/http_handler.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"net/http"
+
+	common "github.com/mrd1920/oms-common"
+	pb "github.com/mrd1920/oms-common/api"
 )
 
 type handler struct {
 	//gateway
+	client pb.OrderServiceClient
 }
 
-func NewHandler() *handler {
-	return &handler{}
+func NewHandler(client pb.OrderServiceClient) *handler {
+	return &handler{
+		client: client,
+	}
 }
 
 func (h *handler) registerRoutes(mux *http.ServeMux) {
@@ -17,5 +23,15 @@ func (h *handler) registerRoutes(mux *http.ServeMux) {
 }
 
 func (h *handler) HandleCreateOrder(w http.ResponseWriter, r *http.Request) {
+	customerID := r.PathValue("customerID")
 
+	var items []*pb.ItemsWithQuantity
+	if err := common.ReadJSON(r, &items); err != nil {
+		common.WriteError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
+		CustomerID: customerID,
+	})
 }
--- gateway/main.go
+++ gateway/main.go
@@ -6,15 +6,29 @@ import (
 
 	_ "github.com/joho/godotenv/autoload"
 	common "github.com/mrd1920/oms-common"
+	pb "github.com/mrd1920/oms-common/api"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var (
-	httpAddr = common.EnvString("HTTP_ADDR", ":3000")
+	httpAddr          = common.EnvString("HTTP_ADDR", ":3000")
+	ordersServiceAddr = "localhost:3000"
 )
 
 func main() {
+	conn, err := grpc.NewClient(ordersServiceAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to dial server: %v", err)
+	}
+	defer conn.Close()
+
+	log.Println("Dialing orders service at ", ordersServiceAddr)
+
+	c := pb.NewOrderServiceClient(conn)
+
 	mux := http.NewServeMux()
-	handler := NewHandler()
+	handler := NewHandler(c)
 	handler.registerRoutes(mux)
 
 	log.Printf("Starting HTTP server at %s", httpAddr)
